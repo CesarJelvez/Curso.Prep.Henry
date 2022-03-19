169a8b0af21bd19d8d8d1a99d36f0d380024a1bc
CesarJelvez <cesarjelvez13@gmail.com>
2022-03-19 03:42:37 -0300

    falta texto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -240,7 +240,7 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra === 'A' || letra === 'E' || letra === 'I' || letra === 'O' || letra === 'U' || letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
-    return 'Es vocal';
+  return 'Es vocal';
   } else if (letra.length>1) {
     return 'Dato incorrecto';
   } else (letra != 'A' || letra != 'E' || letra != 'I' || letra != 'O' || letra != 'U')
