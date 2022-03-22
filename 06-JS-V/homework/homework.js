a14fe5f1a0e162c6a4762ab54c342f464fc2dbd8
CesarJelvez <cesarjelvez13@gmail.com>
2022-03-22 17:14:38 -0300

    Falta detalle de class

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -60,14 +60,15 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+    constructor(nombre, apellido, edad , domicilio) {
       this.nombre = nombre ;
       this.apellido = apellido;
       this.edad = edad ;
       this.domicilio = domicilio;
       this.detalle = function () {
 
-
+        
+        return datos;
       }
     }
     
@@ -78,11 +79,17 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  var Juan = new Persona ('Juan', 'Perez' , 22 , 'Saavedra 123' );
+  return Juan;
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  Persona.prototype.datos = function () {
+
+    return this.nombre + ', ' + this.edad + ' años';
+  } 
 
 }
   
